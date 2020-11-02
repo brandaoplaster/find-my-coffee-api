5c9d217c832ba23bc0c16797f66ff1e086c642fc
brandaoplaster <brandaoplaster@gmail.com>
2020-11-01 21:59:36 -0300

    Including methods in model stores to calculate media and search for nearby stores

--- app/models/store.rb
+++ app/models/store.rb
@@ -2,4 +2,15 @@ class Store < ApplicationRecord
   has_many :ratings
   validates_presence_of :lonlat, :namem, :google_place_id
   validates :google_place_id, uniqueness: true
+
+  scope :within, -> (longitude, latitude, distance_in_lm = 5) {
+    where(%{
+      ST_Distance(lonlat, 'POINT(%f %f)') < %d
+    } % [longitude, latitude, distance_in_lm * 1000])
+  }
+
+  def rating_average
+    return 0 if self.ratings.empty?
+    (self.rating.sum(:value) / self.rating.count).to_i
+  end
 end
